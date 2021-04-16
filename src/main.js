51b75658f2bfbab1e54415d186d966f6d4576bec
Cristian Ruiz <cristianrb235@gmail.com>
2021-04-16 15:38:46 +0200

    Changed google client id

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import GAuth from 'vue3-google-oauth2'
 const app = createApp(App)
 
 app.component('star-rating', VueStarRating.default)
-const gAuthOptions = { clientId: '694520500304-adft61cf9g8235gro18tvrp9kelsck31.apps.googleusercontent.com', 
+const gAuthOptions = { clientId: '694520500304-1dhp53b8i32892l23tm1ntraq0dgapn4.apps.googleusercontent.com', 
                         scope: 'email profile', prompt: 'consent', fetch_basic_profile: false }
 app.use(GAuth, gAuthOptions)
 app.use(router).mount('#app')
